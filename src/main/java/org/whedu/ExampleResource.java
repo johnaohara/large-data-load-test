947d617745b45ee49ce39fab1696b3ede61c59fb
John OHara <johara@redhat.com>
2019-11-14 09:16:53 +0000

    Cache dataset, increase load on writer

--- src/main/java/org/whedu/ExampleResource.java
+++ src/main/java/org/whedu/ExampleResource.java
@@ -8,20 +8,30 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class ExampleResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+
+    private static final String dataSet;
+
+    static {
+
         StringBuffer buffer = new StringBuffer();
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         buffer.append("[{\"id\":1,\"name\":\"tom\",\"sex\":\"f\",\"testCode\":\"12345\"},");
         for(int i = 0 ;i <1000;i++ ) {
             buffer.append("{\"id\":1,\"name\":\"tom\",\"sex\":\"f\",\"testCode\":\"12345\"},");
         }
         buffer.append("{\"id\":1,\"name\":\"tom\",\"sex\":\"f\",\"testCode\":\"12345\"} ]");
-        return buffer.toString();
+
+        dataSet = buffer.toString();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return dataSet;
     }
 }
